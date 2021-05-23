f3f9e22dea251f14d2b1a59a9329b08776c905bf
mdarif6 <md1arif015@gmail.com>
2021-05-23 16:14:02 +0530

    fetched tanay test server
    fetched tanay test server

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate")
 var textInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
+
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text) {
+
+    return serverURL + "?" + "text=" + text
+}
+
+
 function clickHandler(){
  
-    outputDiv.innerText = "ajldjfoq " + textInput.value
+    // outputDiv.innerText = "ajldjfoq " + textInput.value
+
+var inputText = textInput.value
+
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => console.log(json.contents.translated))
+
 }
 
 btnTranslate.addEventListener("click", clickHandler)
