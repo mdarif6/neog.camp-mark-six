73a2791b749c51491f3d51326fc5ed7b6ff6c1bb
mdarif6 <md1arif015@gmail.com>
2021-05-23 23:34:08 +0530

    adding style and readme file
    adding style and readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Minion Language Translation
+
+This app will convert english sentence to minion language.
+
+It is a talk to funny tranlation api.
+
+HTML5, CSS3 and vanilla js hvae been used in this app.
--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-var btnTranslate = document.querySelector("#btn-translate")
-var textInput = document.querySelector("#txt-input")
-var outputDiv = document.querySelector("#output")
+var btnTranslate = document.querySelector(".btn-translate");
+var textInput = document.querySelector(".txt-input");
+var outputDiv = document.querySelector(".output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
-function getTranslationURL(text) {
-
-    return serverURL + "?" + "text=" + text
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
 }
 
-
-function clickHandler(){
- 
-    // outputDiv.innerText = "ajldjfoq " + textInput.value
-
-var inputText = textInput.value
-
-fetch(getTranslationURL(inputText))
-.then(response => response.json())
-.then(json => console.log(json.contents.translated))
-
+function errorHandler(error) {
+  console.log("error occured ", error);
+  alert("somthing wrong with your server");
 }
 
-btnTranslate.addEventListener("click", clickHandler)
+function clickHandler() {
+  var inputText = textInput.value; //taking input
 
-    
+  // calling server for precessing
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickHandler);
